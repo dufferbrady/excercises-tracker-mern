26e9116da4a11e71864b3a0ce240118d24af9443
dufferbrady <92andrew.brady@gmail.com>
2020-04-18 11:38:01 +1000

    added delete and update routes to backend code

--- backend/routes/excercises.js
+++ backend/routes/excercises.js
@@ -9,6 +9,12 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).res.json(`Error: ${err}`))
 })
 
+router.route('/:id').get((req, res) => {
+    Excercise.findById(req.params.id)
+        .then(excercise => res.json(excercise))
+        .catch(err => res.status(400).json(`Error: ${err}`)) 
+})
+
 //add post requests
 router.route('/add').post((req, res) => {
     const username = req.body.username;
@@ -26,6 +32,29 @@ router.route('/add').post((req, res) => {
     newExcercise.save()
         .then(excercise => res.json('Excercise Saved'))
         .catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+router.route('/update/:id').post((req, res) => {
+    console.log(req.params.id)
+    Excercise.findById(req.params.id)
+        .then(excercise => {
+            excercise.username = req.body.username,
+            excercise.description = req.body.description,
+            excercise.duration = Number(req.body.duration),
+            excercise.date = Date.parse(req.body.date)
+
+            excercise.save()
+                .then(excercise => res.json(`Excercise ${req.params.id} has been updated succesfully`))
+                .catch(err => res.status(400).json(`Error: ${err}`))
+        })
+        .catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+//Add delete requests
+router.route('/:id').delete((req, res) => {
+    Excercise.findByIdAndDelete(req.params.id)
+        .then(excercise => res.json(`Excercise ${req.body.description} by ${req.body.username} has been deleted`))
+        .catch(err => res.status(400).json(`Error: ${err}`)) 
 })
 
 //export modules
